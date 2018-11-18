110ead934ff494cbee76d752c63af566cc6c80cb
Iorgen <w.drik@outlook.com>
2018-11-18 13:33:40 -0200

    SignIn and SignUp

--- airbnb-web/src/App.js
+++ airbnb-web/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './styles/global';
+// import './styles/global';
 
 import Routes from './routes';
 
--- airbnb-web/src/routes.js
+++ airbnb-web/src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { isAuthenticated } from './services/auth';
 
 import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
@@ -21,7 +22,7 @@ const PrivateRoute = ({ component: Component, ...rest }) => (
 const Routes = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path='/' component={ () => <h1>Login</h1> } />
+      <Route exact path='/' component={ SignIn } />
       <Route path='/signup' component={ SignUp } />
       <PrivateRoute path='/app' component={ () => <h1>App</h1> } />
       <Route path='*' component={ () => <h1>Page not found</h1> } />
